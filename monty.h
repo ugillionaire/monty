a6dc5105a8f85db66f10a23e40ac5804b5b1fee7
ugillionaire <88339762+ugillionaire@users.noreply.github.com>
2021-12-04 15:30:41 +0100

    monty

---
+++ monty.h
@@ -0,0 +1,67 @@
+#ifndef MONTY_H
+#define MONTY_H
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+extern char *argtoint;
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
+
+
+size_t dlistint_len(const stack_t *h);
+stack_t *add_dnodeint(stack_t **head, const int n);
+int delete_dnodeint_at_index(stack_t **head, unsigned int index);
+stack_t *get_dnodeint_at_index(stack_t *head, unsigned int index);
+stack_t *insert_dnodeint_at_index(stack_t **h, unsigned int idx, int n);
+stack_t *add_dnodeint_end(stack_t **head, const int n);
+
+
+
+void (*get_func(char *str))(stack_t **, unsigned int);
+
+
+void push_func(stack_t **h, unsigned int line_number);
+void pall_func(stack_t **h, unsigned int line_number);
+void nop_func(stack_t **h, unsigned int line_number);
+void pint_func(stack_t **h, unsigned int line_number);
+void pop_func(stack_t **h, unsigned int line_number);
+void swap_func(stack_t **h, unsigned int line_number);
+void add_func(stack_t **h, unsigned int line_number);
+void sub_func(stack_t **h, unsigned int line_number);
+void div_func(stack_t **h, unsigned int line_number);
+void mul_func(stack_t **h, unsigned int line_number);
+void mod_func(stack_t **h, unsigned int line_number);
+#endif
---
+++ opcode_funcs.c
@@ -0,0 +1,112 @@
+#include "monty.h"
+
+/**
+ * push_func - pushes element onto the stack
+ * @h: pointer to stack_t object
+ * @line_number: line number in file
+ */
+
+void push_func(stack_t **h, unsigned int line_number)
+{
+	stack_t *newnode;
+	int i;
+
+	if (argtoint == NULL)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	i = 0;
+	while (argtoint[i])
+	{
+		if (isdigit(argtoint[i]) == 0)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+	newnode = malloc(sizeof(stack_t));
+	if (!newnode)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	line_number = line_number;
+	newnode->n = atoi(argtoint);
+
+	if (!*h)
+		*h = newnode;
+	else
+	{
+		(*h)->prev = newnode;
+		newnode->next = *h;
+		*h = newnode;
+	}
+}
+
+/**
+ * pall_func - pushes element onto the stack
+ * @h: pointer to stack_t object
+ * @line_number: line number in file
+ */
+
+void pall_func(stack_t **h, unsigned int line_number)
+{
+	(void)line_number;
+	if (!*h)
+		return;
+
+	while (*h)
+	{
+		fprintf(stdout, "%d\n", (*h)->n);
+		(*h) = (*h)->next;
+	}
+}
+
+/**
+ * nop_func - pushes element onto the stack
+ * @h: pointer to stack_t object
+ * @line_number: line number in file
+ */
+void nop_func(stack_t **h, unsigned int line_number)
+{
+	(void)*h;
+	(void)line_number;
+}
+
+/**
+ * pint_func - prints the element on top
+ * @h: pointer to stack_t object
+ * @line_number: line number in file
+ */
+void pint_func(stack_t **h, unsigned int line_number)
+{
+	stack_t *head = *h;
+
+	if (!head)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", head->n);
+}
+
+/**
+ * pop_func - removes the element on top
+ * @h: pointer to stack_t object
+ * @line_number: line number in file
+ */
+void pop_func(stack_t **h, unsigned int line_number)
+{
+	stack_t *temp = *h;
+
+	if (!temp)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	delete_dnodeint_at_index(h, 0);
+}
